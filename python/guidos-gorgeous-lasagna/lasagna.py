d3e19816d91f4a7e3c63abf8dc6c50d5c489cc85
abdallaha <a.alqudah3@gmail.com>
2026-05-04 10:55:33 +0200

    Solve lasanga ex, added the terminal comands

---
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,59 @@
+"""Functions used in preparing Guido's gorgeous lasagna.
+
+Learn about Guido, the creator of the Python language:
+https://en.wikipedia.org/wiki/Guido_van_Rossum
+
+This is a module docstring, used to describe the functionality
+of a module and its functions and/or classes.
+"""
+
+
+
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
+
+
+def bake_time_remaining(elapsed_bake_time):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+
+
+def preparation_time_in_minutes(number_of_layers):
+    """Calculate the preparation time in minutes.
+
+    :param number_of_layers: int - number of layers to prepare.
+    :return: int - preparation time in minutes derived from 'PREPARATION_TIME'.
+
+    Function that takes the number of layers you want to prepare as an argument
+    and returns how many minutes you would spend making them based on the
+    `PREPARATION_TIME` per layer.
+    """
+
+    return PREPARATION_TIME * number_of_layers
+
+
+
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculate the elapsed time in minutes.
+
+    :param number_of_layers: int - number of layers to prepare.
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - elapsed time in minutes derived from 'preparation_time_in_minutes()' and 'elapsed_bake_time'.
+
+    Function that takes the number of layers you want to prepare and the actual
+    minutes the lasagna has been in the oven as arguments and returns how many
+    minutes have been spent cooking the lasagna.
+    """
+
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
